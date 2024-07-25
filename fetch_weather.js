f5f0cf2a315dde736373ddbb3d3d1dde7effcee1
OmmprakashMohanty01 <ommmohanty419@gmail.com>
2024-07-25 13:23:00 +0530

    update

---
+++ fetch_weather.js
@@ -0,0 +1,9 @@
+// Fetching weather data
+
+
+function fetchData(url) {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+}
