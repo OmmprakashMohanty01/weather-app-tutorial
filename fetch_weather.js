8b52db4a9ea21ec9a09885533b05c0bf0a7a55fc
OmmprakashMohanty01 <ommmohanty419@gmail.com>
2024-08-10 17:58:00 +0530

    added more stuff

--- fetch_weather.js
+++ fetch_weather.js
@@ -7,3 +7,11 @@ function fetchData(url) {
     .then(data => console.log(data))
     .catch(error => console.error('Error:', error));
 }
+
+function getWeatherByLocation(location, apiKey) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
+  fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+}
