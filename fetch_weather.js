e6d9bf9638cb72e9e2c0e2dc68b2e5a3b9d79196
OmmprakashMohanty01 <ommmohanty419@gmail.com>
2025-02-10 17:12:00 +0530

    maybe this will fix it

--- fetch_weather.js
+++ fetch_weather.js
@@ -31,3 +31,25 @@ function getMultipleWeatherLocations(locations, apiKey) {
     .then(data => console.log(data))
     .catch(error => console.error('Error:', error));
 }
+
+function getWeatherForecastByLocation(location, apiKey) {
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${apiKey}&units=metric`;
+  fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+}
+function getWeatherForecastByCoordinates(latitude, longitude, apiKey) {
+  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+}
+function getWeatherForecastByMultipleLocations(locations, apiKey) {
+  const url = `https://api.openweathermap.org/data/2.5/group?cnt=10&id=${locations.join(',')}&appid=${apiKey}&units=metric&dt=2160`;
+  fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+}
